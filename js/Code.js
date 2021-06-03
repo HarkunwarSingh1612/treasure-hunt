10c161f7f568020cf77bf7ba9a8dde7e9e0da9f5
HarkunwarSingh1612 <harnitsaini2012@gmail.com>
2021-06-03 19:09:12 +0530

    treasure hunt

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("NCUOTFIN", 700,160)
+fill("lightblue")
+text("Hint: performs a particular task !!", 700,180)
+
+fill("white")
+textSize(15)
+text("EBASATDA", 100,260)
+fill("lightblue")
+text("Hint: stores all information!!", 100,280)
+
+}
